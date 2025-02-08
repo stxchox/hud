cdfbfc0d7e93316ffe61bb7924393a838329a9c1
stxchox <ce.meissner@icloud.com>
2025-02-08 13:59:16 +0100

    update lib.lua
    downloading improvements

--- library.lua
+++ library.lua
@@ -84,11 +84,15 @@ end
   if not isfolder("PPHUD") then
     makefolder("PPHUD")
 
-    local Arrow = request({Url = "https://raw.githubusercontent.com/stxchox/hud/main/dropdown.png", Method = "GET"})
-    writefile("PPHUD/Arrow.png", Arrow.Body)
+    if not isfile("PPHUD/Arrow.png") then
+        local Arrow = request({Url = "https://raw.githubusercontent.com/stxchox/hud/main/dropdown.png", Method = "GET"})
+        writefile("PPHUD/Arrow.png", Arrow.Body)
+    end
 
-    local Resize = request({Url = "https://raw.githubusercontent.com/stxchox/hud/main/resize.png", Method = "GET"})
-    writefile("PPHUD/Resize.png", Resize.Body)
+    if not isfile("PPHUD/Resize.png") then
+        local Resize = request({Url = "https://raw.githubusercontent.com/stxchox/hud/main/resize.png", Method = "GET"})
+        writefile("PPHUD/Resize.png", Resize.Body)
+    end
   end
   --//
   
